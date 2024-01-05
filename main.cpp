43d2b3a28510d719ec8e060b5d631b247f9c390b
Ashwinn K <ashwinnraj@noreply.github.com>
2024-01-05 00:38:03 -0500

    Extracting Input
    + Created parser, extractLines & printBoard functions.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 //
-// Created by Ashwinn on 12/29/23.
+// Created by Ashwinn.
 // This program demonstrates the working of A* search for finding a shortest path on a nxn grid.
 //
+
 #include "utility.hpp"
 #include <iostream>
+#include <vector>
+#include <tuple>
 
 
 int main() {
 
+    //get input from file
+    std::string file_path = "input.txt";
+    std::cout << "Input Board" << std::endl;
+    std::vector<std::vector<utility::labels>> board = utility::extractLines(file_path, std::tuple<int, int>{2, 2}, std::tuple<int, int>{0, 0});
+    utility::printBoard(board);
 }
--- utility.hpp
+++ utility.hpp
@@ -1,14 +1,32 @@
 //
-// Created by user on 12/29/23.
+// Created by Ashwinn.
+// Utility library for use with main.cpp. Contains all the functions definitions.
 //
 
 #ifndef A_UTILITY_HPP
 #define A_UTILITY_HPP
 
+#include <vector>
+#include <string>
 
 
 namespace utility {
+    enum struct labels {
+        terrain = 0,
+        open_path = 1,
+        closed_path = 2,
+        pathway = 3,
+        start = 4,
+        goal = 5
+    };
 
+    std::vector<labels> parseLine(std::string &some_string);
+
+    std::vector<std::vector<labels>> extractLines(const std::string &some_path,
+                                                  std::tuple<int, int> startPosition,
+                                                  std::tuple<int, int> endPosition);
+
+    void printBoard(const std::vector<std::vector<labels>> &some_board);
 
 }
 
